59f8ecaf2ae677abd2a96beb9926957682e65beb
fotis tsakiris <footios76@gmail.com>
2020-07-15 14:21:16 +0300

    Register form

--- src/components/Auth/Register.js
+++ src/components/Auth/Register.js
@@ -1,9 +1,76 @@
 import React, { Component } from "react";
+import {
+  Grid,
+  Form,
+  Segment,
+  Button,
+  Header,
+  Message,
+  Icon
+} from "semantic-ui-react";
+import { Link } from "react-router-dom";
 
 class Register extends Component {
   state = {};
+
+  handleChange = () => {};
   render() {
-    return <div>Register</div>;
+    return (
+      <Grid textAlign="center" verticalAlign="bottom" className="app">
+        <Grid.Column style={{ maxWidth: 450 }}>
+          <Header as="h2" icon color="orange" textAlign="center">
+            <Icon name="puzzle piece" color="orange" />
+            Register for DevChat
+          </Header>
+          <Form size="large">
+            <Segment stacked>
+              <Form.Input
+                fluid
+                name="username"
+                icon="user"
+                iconPosition="left"
+                placeholder="Username"
+                onChange={this.handleChange}
+                type="text"
+              />
+              <Form.Input
+                fluid
+                name="username"
+                icon="email"
+                iconPosition="left"
+                placeholder="Email Address"
+                onChange={this.handleChange}
+                type="text"
+              />
+              <Form.Input
+                fluid
+                name="username"
+                icon="lock"
+                iconPosition="left"
+                placeholder="Password"
+                onChange={this.handleChange}
+                type="password"
+              />
+              <Form.Input
+                fluid
+                name="username"
+                icon="repeat"
+                iconPosition="left"
+                placeholder="Password Confirmation"
+                onChange={this.handleChange}
+                type="password"
+              />
+              <Button color="orange" fluid size="large">
+                Submit
+              </Button>
+            </Segment>
+          </Form>
+          <Message>
+            Already a user? <Link to="/login">Login</Link>{" "}
+          </Message>
+        </Grid.Column>
+      </Grid>
+    );
   }
 }
 
--- src/index.js
+++ src/index.js
@@ -1,11 +1,10 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import App from "./components/App";
 import registerServiceWorker from "./registerServiceWorker";
-
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import 'semantic-ui-css/semantic.min.css'
 
-import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import App from "./components/App";
 import Login from "./components/Auth/Login";
 import Register from "./components/Auth/Register";
 
