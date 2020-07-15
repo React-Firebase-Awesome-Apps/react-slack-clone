f4a1c94464ea516d82806261c4f7c611d7be1ce8
fotis tsakiris <footios76@gmail.com>
2020-07-15 16:07:12 +0300

    form validation and error messages

--- src/components/Auth/Register.js
+++ src/components/Auth/Register.js
@@ -17,8 +17,12 @@ class Register extends Component {
     username: "",
     email: "",
     password: "",
-    passwordConfirmation: ""
+    passwordConfirmation: "",
+    errors: []
   };
+
+  displayErrors = errors =>
+    errors.map((error, i) => <p key={i}>{error.message}</p>);
 
   handleChange = event => {
     this.setState({
@@ -26,20 +30,56 @@ class Register extends Component {
     });
   };
 
+  isFormValid = () => {
+    let errors = [];
+    let error;
+    if (!this.isFormEmpty(this.state)) {
+      error = { message: "Fill in all fields!" };
+      this.setState({ errors: errors.concat(error) });
+      return false;
+    } else if (!this.isPasswordValid(this.state)) {
+      error = { message: "Password is invalid!" };
+      this.setState({ errors: errors.concat(error) });
+      return false;
+    } else {
+      return true;
+    }
+  };
+  isFormEmpty = ({ username, email, password, passwordConfirmation }) => {
+    return (
+      username.length ||
+      email.length ||
+      password.length ||
+      passwordConfirmation.length
+    );
+  };
+  isPasswordValid = ({ password, passwordConfirmation }) => {
+    if (password.length < 6 || passwordConfirmation < 6) {
+      return false;
+    } else if (password !== passwordConfirmation) {
+      return false;
+    } else {
+      return true;
+    }
+  };
+
   handleSubmit = event => {
-    event.preventDefault();
-    firebase.auth()
-      .createUserWithEmailAndPassword(this.state.email, this.state.password)
-      .then(user => {
-        console.log("user", { user });
-      })
-      .catch(err => {
-        console.error(err);
-      });
+    if (this.isFormValid()) {
+      event.preventDefault();
+      firebase
+        .auth()
+        .createUserWithEmailAndPassword(this.state.email, this.state.password)
+        .then(user => {
+          console.log("user", { user });
+        })
+        .catch(err => {
+          console.error(err);
+        });
+    }
   };
 
   render() {
-    const { username, email, password, passwordConfirmation } = this.state;
+    const { username, email, password, passwordConfirmation, errors } = this.state;
     return (
       <Grid textAlign="center" verticalAlign="bottom" className="app">
         <Grid.Column style={{ maxWidth: 450 }}>
@@ -94,6 +134,12 @@ class Register extends Component {
               </Button>
             </Segment>
           </Form>
+          {errors.length > 0 && (
+            <Message error>
+              <h3>Error</h3>
+              {this.displayErrors(errors)}
+            </Message>
+          )}
           <Message>
             Already a user? <Link to="/login">Login</Link>{" "}
           </Message>
