a3888344e5c1380ad5dfad75a4bcd703822b2a96
fotis tsakiris <footios76@gmail.com>
2020-07-16 18:16:56 +0300

    onAuthStateChange and redirect user to homepage

--- src/index.js
+++ src/index.js
@@ -1,22 +1,45 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import registerServiceWorker from "./registerServiceWorker";
-import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
-import 'semantic-ui-css/semantic.min.css'
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  withRouter
+} from "react-router-dom";
+import "semantic-ui-css/semantic.min.css";
 
 import App from "./components/App";
 import Login from "./components/Auth/Login";
 import Register from "./components/Auth/Register";
+import firebase from "../src/firebase";
 
-const Root = () => (
+class Root extends React.Component {
+  componentDidMount() {
+    firebase.auth().onAuthStateChanged(user => {
+      if (user) {
+        this.props.history.push("/");
+      }
+    });
+  }
+
+  render() {
+    return (
+      <Switch>
+        <Route path="/" exact component={App} />
+        <Route path="/login" component={Login} />
+        <Route path="/register" component={Register} />
+      </Switch>
+    );
+  }
+}
+
+const RoutWithAuth = withRouter(Root);
+
+ReactDOM.render(
   <Router>
-    <Switch>
-      <Route path="/" exact component={App} />
-      <Route path="/login" component={Login} />
-      <Route path="/register" component={Register} />
-    </Switch>
-  </Router>
+    <RoutWithAuth />
+  </Router>,
+  document.getElementById("root")
 );
-
-ReactDOM.render(<Root />, document.getElementById("root"));
 registerServiceWorker();
